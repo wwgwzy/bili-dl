e90270b627e5d25391883292db2e97f80c97e382
wwgwzy <wwgwzy9527@foxmail.com>
2020-01-18 17:52:51 +0800

    666

--- bili_dl.py
+++ bili_dl.py
@@ -5,15 +5,16 @@ class bili_up_dl():
     def __init__(self, upid):
         self.upid = upid
     
-    def dump_page_url(self, upid):
+    def dump_page_url(self):
         for page in range(1, 51):
-            page_url = "https://space.bilibili.com/%s/video?tid=0&page=%s&keyword=&order=pubdate"%(upid, page)
-            
+            page_url = "https://space.bilibili.com/%s/video?tid=0&page=%s&keyword=&order=pubdate"%(self.upid, page)
+            yield page_url
 
 def main():
 
     upid = "35359510"
     aUpdl = bili_up_dl(upid)
-    aUpdl.
+    print(next(aUpdl.dump_page_url()))
+
 
 main()
